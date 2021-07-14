ba2dbd638c97c1d361d80e3b84027de5a9642d0f
averoli <64157643+averoli@users.noreply.github.com>
2021-07-14 17:57:52 +0200

    responsive

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
 $(document).ready(function () {
     $('.slider').slick({
-        dots: false,
-        arrows: false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 2,
+        // dots: true,
+        // arrows: false,
+        // infinite: false,
+        // speed: 300,
+        // slidesToShow: 3,
+        // slidesToScroll: 2,
         responsive: [
             {
                 breakpoint: 768,
                 settings: {
                     dots: true,
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
                     infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 375,
                 settings: {
+                    dots: true,
+                    infinite: true,
                     slidesToShow: 1,
                     slidesToScroll: 1
+
                 }
             }
         ]
