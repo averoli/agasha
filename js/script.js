1b6ad7c7f1ec43f25d5bc81a3f1bc438bd93fe05
Ishutin Pavel <ishutin-pavel@mail.ru>
2021-07-15 12:35:22 +0300

    ipavel

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 $(document).ready(function () {
-    $('.slider').slick({
-        // dots: true,
-        // arrows: false,
-        // infinite: false,
-        // speed: 300,
-        // slidesToShow: 3,
-        // slidesToScroll: 2,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    dots: true,
-                    infinite: true,
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 375,
-                settings: {
-                    dots: true,
-                    infinite: true,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    centerMode: true,
+    centerPadding: '0px',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          centerPadding: '0px',
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '10px'
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
-                }
-            }
-        ]
-    });
 });
